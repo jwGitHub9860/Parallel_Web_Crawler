f73d11b792bdde768f05bb3d0c690f5132a8882e
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-12 15:41:51 -0400

    refactor: Refactoring production code
    Add all work previously done in "ProfilingMethodInterceptor.java" java file in first version of project to "ProfilingMethodInterceptor.java" java file to be able to show work that has been done

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -1,8 +1,12 @@
 package com.udacity.webcrawler.profiler;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
 /**
@@ -12,19 +16,62 @@ import java.util.Objects;
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
+  private final Object delegate;
+  private final ProfilingState state;
+  private final ZonedDateTime startTime;
 
   // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock) {
+  ProfilingMethodInterceptor(Clock clock, Object delegate, ProfilingState state, ZonedDateTime startTime) {
     this.clock = Objects.requireNonNull(clock);
+    this.delegate = Objects.requireNonNull(delegate);
+    this.state = Objects.requireNonNull(state);
+    this.startTime = Objects.requireNonNull(startTime);
   }
 
   @Override
-  public Object invoke(Object proxy, Method method, Object[] args) {
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable { // use "Throwable" to handle unexpected events & prevent program crashes
     // TODO: This method interceptor should inspect the called method to see if it is a profiled
     //       method. For profiled methods, the interceptor should record the start time, then
     //       invoke the method using the object that is being profiled. Finally, for profiled
     //       methods, the interceptor should record how long the method call took, using the
     //       ProfilingState methods.
-    return null;
+
+    // Initializes and/or Resets "startTime"
+    Instant startTime = null;
+
+    // Obtains Method Annotation
+    Profiled annotation = method.getAnnotation(Profiled.class); // will be used to Determine if Annotation Has Been Profiled Or Not
+
+    // Return Value for Method
+    Object currentInvokeMethod = null;
+
+    // Checks if All Methods Have Been Profiled
+    if (annotation != null) {
+      try {
+        // Records Start Time As Current Time
+        startTime = clock.instant();
+
+        // Calls "delegate" Class Using ".invoke()"
+        currentInvokeMethod = method.invoke(delegate, args);
+      }
+      // "InvocationTargetException" - wraps exception thrown by Invoked Method or Constructor
+      catch (InvocationTargetException ex) { // MUST SPECIFICALLY CATCH "InvocationTargetException" exceptions TO PREVENT BUILD ERRORS
+        throw ex.getTargetException();
+      } catch (IllegalArgumentException e) { // MUST SPECIFICALLY CATCH "IllegalAruguementException" exceptions TO PREVENT BUILD ERRORS
+        throw new RuntimeException(e);
+      } finally {
+        // Measures Duration of "method" Invocation
+        Duration methodDuration = Duration.between(startTime, clock.instant());
+
+        // Records How Long Method Call Took
+        state.record(delegate.getClass(), method, methodDuration);
+      }
+
+      // Returns Invoked Method
+      return currentInvokeMethod;
+    } else {
+      // Calls & Returns "delegate" Class Using ".invoke()"
+      return method.invoke(delegate, args);
+    }
   }
 }
