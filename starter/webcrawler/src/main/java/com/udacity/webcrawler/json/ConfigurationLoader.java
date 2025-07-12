310f8fefd134df1ab1b3463b376a34ebb74cce5f
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-12 15:07:18 -0400

    refactor: Refactoring production code
    Add all work previously done in "ConfigurationLoader.java" java file in first version of project to "ConfigurationLoader.java" java file to be able to show work that has been done

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/ConfigurationLoader.java
@@ -1,5 +1,12 @@
 package com.udacity.webcrawler.json;
 
+// Creates new "com.fasterxml.jackson.databind.ObjectMapper"
+import com.fasterxml.jackson.databind.ObjectMapper; // to implement "CrawlerConfiguration read(Reader reader)" method
+
+// Creates new "com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE"
+import com.fasterxml.jackson.core.JsonParser;
+
+import java.nio.file.Files;
 import java.io.Reader;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -23,10 +30,17 @@ public final class ConfigurationLoader {
    *
    * @return the loaded {@link CrawlerConfiguration}.
    */
+  // Reads JSON string from "Path" File
   public CrawlerConfiguration load() {
     // TODO: Fill in this method.
 
-    return new CrawlerConfiguration.Builder().build();
+    // Creates "Reader" (JSON string) from "Path" file
+    try (Reader reader = Files.newBufferedReader(path)) { // "Files.newBufferedReader()" - creates reader (JSON string) from "Path" file
+      // Passes "reader" (JSON string) into "read(Reader reader)" method & Returns "CrawlerConfiguration" Result
+      return read(reader);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 
   /**
@@ -35,11 +49,29 @@ public final class ConfigurationLoader {
    * @param reader a Reader pointing to a JSON string that contains crawler configuration.
    * @return a crawler configuration
    */
+  // Reads JSON input & De-serializes (or Parses) it into "CrawlerConfiguration" using Jackson JSON Library
   public static CrawlerConfiguration read(Reader reader) {
     // This is here to get rid of the unused variable warning.
     Objects.requireNonNull(reader);
     // TODO: Fill in this method
+    try {
+      // Serialize Java objects into JSON & Creates "ObjectMapper" Instance
+      ObjectMapper objectMapper = new ObjectMapper(); // creates new Jackson "objectMapper"
 
-    return new CrawlerConfiguration.Builder().build();
+      // Disables "com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE" to Prevent Jackson Library from Closing Input "Reader"
+      objectMapper.disable(JsonParser.Feature.AUTO_CLOSE_SOURCE);
+
+      // Creates "CrawlerConfiguration" Instance
+      CrawlerConfiguration jsonInput = objectMapper
+              // Calls ".readValue()" for "ObjectMapper"
+              .readValue(Objects.requireNonNull(reader), CrawlerConfiguration.Builder.class) // to implement "CrawlerConfiguration read(Reader reader)" method
+              // Returns Final "ObjectMapper" Instance AS Last Step in Creating "ObjectMapper" Instance
+              .build();
+
+      // Returns "ObjectMapper" Instance to "load()" method
+      return jsonInput;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
