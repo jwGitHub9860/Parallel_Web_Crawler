8272c825d20275b87b0639764e0d4d11d471d2ae
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-12 15:28:23 -0400

    refactor: Refactoring production code
    Add all work previously done in "ParallelWebCrawler.java" java file in first version of project to "ParallelWebCrawler.java" java file to be able to show work that has been done

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/ParallelWebCrawler.java
@@ -1,18 +1,19 @@
 package com.udacity.webcrawler;
 
 import com.udacity.webcrawler.json.CrawlResult;
+import com.udacity.webcrawler.parser.PageParser;
+import com.udacity.webcrawler.parser.PageParserFactory;
 
 import javax.inject.Inject;
-import javax.inject.Provider;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ForkJoinPool;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 /**
@@ -22,16 +23,25 @@ import java.util.stream.Collectors;
 final class ParallelWebCrawler implements WebCrawler {
   private final Clock clock;
   private final Duration timeout;
+  private final List<Pattern> ignoredUrls;
+  private final PageParserFactory parserFactory;
+  private final int maxDepth;
   private final int popularWordCount;
   private final ForkJoinPool pool;
 
   @Inject
   ParallelWebCrawler(
-      Clock clock,
-      @Timeout Duration timeout,
-      @PopularWordCount int popularWordCount,
-      @TargetParallelism int threadCount) {
+          Clock clock,
+          @IgnoredUrls List<Pattern> ignoredUrls, // @IgnoredUrls - CUSTOM ANNOTATION that Ignores Particular Tests Or Groups To Skip Build Failure
+          PageParserFactory parserFactory,
+          @MaxDepth int maxDepth, // @MaxDepth - Specifies Maximum Depth which Object Should Be Serialized
+          @Timeout Duration timeout,
+          @PopularWordCount int popularWordCount,
+          @TargetParallelism int threadCount) {
     this.clock = clock;
+    this.ignoredUrls = ignoredUrls;
+    this.parserFactory = parserFactory;
+    this.maxDepth = maxDepth;
     this.timeout = timeout;
     this.popularWordCount = popularWordCount;
     this.pool = new ForkJoinPool(Math.min(threadCount, getMaxParallelism()));
@@ -39,7 +49,39 @@ final class ParallelWebCrawler implements WebCrawler {
 
   @Override
   public CrawlResult crawl(List<String> startingUrls) {
-    return new CrawlResult.Builder().build();
+    // Obtains Current Time with "timeout"
+    Instant deadline = clock.instant().plus(timeout);
+
+    // Creates "ConcurrentHashMap" to hold Threads of URLs & Amount of Times they were Accessed
+    ConcurrentHashMap<String, Integer> countUrls = new ConcurrentHashMap<>(); // did NOT Use "HashMap" because it's NOT Thread-Safe
+
+    // Creates "ConcurrentSkipListSet" to hold Threads of Each URL Accessed
+    ConcurrentSkipListSet<String> accessedUrls = new ConcurrentSkipListSet<>(); // Sorted Set that Safely Accesses & Modifys Multiple Threads AT THE SAME TIME
+
+    // Fills "countUrls" & "accessedUrls"
+    for (String url : startingUrls) {
+      // Calls "CrawlRecursiveTask" Constructor to create "CrawlRecursiveTask" instance Using ".invoke()"
+      pool.invoke(new CrawlRecursiveTask(url, deadline, maxDepth, countUrls, accessedUrls, clock, parserFactory, ignoredUrls));
+    }
+
+    // Checks if "countUrls" is EMPTY
+    if (countUrls.isEmpty()) {
+      return new CrawlResult.Builder()
+              // Sets & Sorts URLs (words) & Amount of Times they were Accessed
+              .setWordCounts(countUrls)
+              // Sets & Sorts Amount of Times URLs were Accessed
+              .setUrlsVisited(accessedUrls.size())
+              // Returns Final "CrawlResult" Instance AS Last Step in Creating "CrawlResult" Instance
+              .build();
+    }
+
+    return new CrawlResult.Builder()
+            // Sets & Sorts URLs (words) & Amount of Times they were Accessed
+            .setWordCounts(WordCounts.sort(countUrls, popularWordCount))
+            // Sets & Sorts Amount of Times URLs were Accessed
+            .setUrlsVisited(accessedUrls.size())
+            // Returns Final "CrawlResult" Instance AS Last Step in Creating "CrawlResult" Instance
+            .build();
   }
 
   @Override
