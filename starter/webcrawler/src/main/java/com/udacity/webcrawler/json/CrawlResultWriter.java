62a4184abd2dea4e42b9b7cf9f714d22c4c474e9
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2025-07-15 21:18:29 -0400

    fix: A bug fix
    Add code that changes when stream closes for "ObjectMapper" inside "CrawlResultWriter.java" java file to disable "OutputStream" from automatically closing after ".writeValue()" is used for "ObjectMapper" to fix error of stream not being able to close for "CrawlResultWriterTest"

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlResultWriter.java
@@ -58,8 +58,11 @@ public final class CrawlResultWriter {
       // Creates "ObjectMapper" Instance & Serialize Java objects into JSON
       ObjectMapper objectMapper = new ObjectMapper();
 
-      // Disables "com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE" to Prevent Jackson Library from Closing Input "Reader"
+      // Disables "com.fasterxml.jackson.core.JsonParser.Feature.AUTO_CLOSE_SOURCE" to Prevent Jackson Library from Closing Input
       objectMapper.disable(JsonParser.Feature.AUTO_CLOSE_SOURCE);
+
+      // Disables "OutputStream" from Automatically Closing After ".writeValue()" is Used for "ObjectMapper"
+      objectMapper.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
 
       // Calls ".writeValue()" for "objectMapper" to Write to "result" File
       objectMapper.writeValue(Objects.requireNonNull(writer), result); // "result" File is DEFINED in "CrawlResultWriter" constructor
